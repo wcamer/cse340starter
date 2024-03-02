74b8e81091d81431b9096a61f62f9bb7a3f333e3
wcamer <xeus.xiii@hotmail.com>
2024-03-02 09:30:24 -0700

    site creation

--- server.js
+++ server.js
@@ -5,15 +5,31 @@
 /* ***********************
  * Require Statements
  *************************/
+const expressLayouts = require("express-ejs-layouts")
 const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
+//
+
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout")
+
+
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+//Index route
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
+})
 
 /* ***********************
  * Local Server Information
