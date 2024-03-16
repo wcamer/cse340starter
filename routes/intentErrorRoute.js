a53ab7ec5fb9182822a7b817667767496f567b34
wcamer <xeus.xiii@hotmail.com>
2024-03-15 20:47:50 -0600

    part of successful 505 error but this is the route  and comments were left in of incomplete attempts

--- routes/intentErrorRoute.js
+++ routes/intentErrorRoute.js
@@ -1,24 +1,4 @@
-//Needed resources 
-// const express = require("express")
-// const router = new express.Router()
-// const invController = require("../controllers/invController")
-//const broke = require("../controllers/intentErrorController")
 
-// console.log("$$$$$$$$$$$$$$$$$$$$$$$$")
-// router.get("/detail/:inv_Id", broke.imGonnaBreak);
-
-// module.exports = router;
-
-
-// const express = require("express")
-// const router = new express.Router()
-// const invController = require("../controllers/invController")
-// console.log("||||||||||||||||||||||||||||||||| it happenen")
-//router.get("/detail/:inv_Id", invController.imGonnaBreak)
-
-// router.get("/partials/footer", broke.imGonnaBreak)
-//router.get("/detail/:inv_Id", invController.buildByInv_Id);
-//router.get("/detail/:inv_Id, broke.imGonnaBreak")
 
 const utilities = require("../utilities/")
 const baseController = {}
